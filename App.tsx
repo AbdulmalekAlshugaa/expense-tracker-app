81a8f3a8cbb1f3682142f76ff445dcfad05bfe0b
Abdulmalik Alshugaa <alshugaa19@gmail.com>
2022-10-07 01:40:56 +0800

    complete home page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { store } from "./src/store";
+import { Provider } from "react-redux";
+import Home from "./src/Screen/Home";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Home />
+
+    
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- assets/constants/icons.ts
+++ assets/constants/icons.ts
@@ -1,17 +1,20 @@
-export const baby_car = require("../assets/icons/baby_car_icon.png");
-export const back_arrow = require("../assets/icons/back_arrow_icon.png");
-export const calendar = require("../assets/icons/calendar_icon.png");
-export const chart = require("../assets/icons/chart_icon.png");
-export const cloth_icon = require("../assets/icons/cloth_icon.png");
-export const down_arrow = require("../assets/icons/down_arrow.png");
-export const education = require("../assets/icons/education_icon.png");
-export const food = require("../assets/icons/food_icon.png");
-export const healthcare = require("../assets/icons/healthcare_icon.png");
-export const menu = require("../assets/icons/menu_icon.png");
-export const more = require("../assets/icons/more_icon.png");
-export const pin = require("../assets/icons/pin.png");
-export const sports_icon = require("../assets/icons/sports_icon.png");
-export const up_arrow = require("../assets/icons/up_arrow.png");
+
+
+export const baby_car = require("../icons/baby_car_icon.png");
+
+export const back_arrow = require("../icons/back_arrow_icon.png");
+export const calendar = require("../icons/calendar_icon.png");
+export const chart = require("../icons/chart_icon.png");
+export const cloth_icon = require("../icons/cloth_icon.png");
+export const down_arrow = require("../icons/down_arrow.png");
+export const education = require("../icons/education_icon.png");
+export const food = require("../icons/food_icon.png");
+export const healthcare = require("../icons/healthcare_icon.png");
+export const menu = require("../icons/menu_icon.png");
+export const more = require("../icons/more_icon.png");
+export const pin = require("../icons/pin.png");
+export const sports_icon = require("../icons/sports_icon.png");
+export const up_arrow = require("../icons/up_arrow.png");
 
 export default {
     baby_car,
