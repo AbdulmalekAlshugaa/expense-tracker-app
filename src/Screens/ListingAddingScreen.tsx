75149719c300d5ec3bd470b7d8b3c77a745cdb5f
Abdulmalik Alshugaa <alshugaa19@gmail.com>
2022-10-07 18:24:50 +0800

    seup formik

--- src/Screens/ListingAddingScreen.tsx
+++ src/Screens/ListingAddingScreen.tsx
@@ -12,13 +12,22 @@ import {
   ScrollView,
   SafeAreaView
 } from "react-native";
-
+import * as Yup from "yup";
 import React, { useRef, useEffect, useCallback } from "react";
 import { COLORS, FONTS, icons, SIZES } from "../../assets/constants";
 import * as Svg from "react-native-svg";
 import { VictoryPie } from "victory-native";
 import Header from "../components/Header";
+import AppForm from "../components/forms/AppForm";
 
+import AppTextInput from "../components/AppTextInput";
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(1).label("Title"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
+  description: Yup.string().label("Description"),
+  category: Yup.object().required().nullable().label("Category"),
+  
+});
 export interface HomeProps {
   navigation: any;
 }
@@ -33,6 +42,37 @@ const ListingAddingScreen: React.FC<HomeProps> = (props) => {
         leftIcon={icons.back_arrow}
         rightIcon={icons.menu}
       />
+
+      <AppForm
+        initialValues={{
+          title: "",
+          price: "",
+          description: "",
+          category: null,
+        }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+
+      >
+
+       <AppTextInput
+
+          name="title"
+          placeholder="Title"
+          icon="format-title"
+          width="100%"
+          keyboardType="default"
+          autoCapitalize="none"
+          autoCorrect={false}
+          textContentType="none"
+          returnKeyType="next"
+          returnKeyLabel="next"
+
+       />
+        
+
+      </AppForm>
+       
       
 
       
